82a90fe4e81bfbbca147cc6fc8a0b3ac374b075f
Tomoaki mbp13r-1 <tomoaki.nagahara@gmail.com>
2019-04-29 19:30:33 +0900

    New: Tax.class.php

---
+++ Tax.class.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * unit-japan:/Tax.class.php
+ *
+ * @creation  2018-10-05
+ * @version   1.0
+ * @package   unit-japan
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/** namespace
+ *
+ * @creation  2018-10-05
+ */
+namespace OP\UNIT\JAPAN;
+
+/** Tax
+ *
+ * @creation  2018-10-05
+ * @version   1.0
+ * @package   unit-japan
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+class Tax
+{
+	/** trait
+	 *
+	 */
+	use \OP_CORE;
+
+	/** Get Japanese tax rate.
+	 *
+	 */
+	function Rate($date)
+	{
+		$rate = null;
+		return $rate;
+	}
+}
