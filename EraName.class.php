d30971974ec9c335a5afc0c1e40e99f26fec89a8
Tomoaki mbp13r-1 <tomoaki.nagahara@gmail.com>
2019-04-29 19:31:44 +0900

    New: EraName.class.php

---
+++ EraName.class.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * unit-japan:/EraName.class.php
+ *
+ * @creation  2018-10-05
+ * @version   1.0
+ * @package   unit-japan
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/** namespace
+ *
+ * @creation  2018-10-05
+ */
+namespace OP\UNIT\JAPAN;
+
+/** EraName
+ *
+ * @creation  2018-10-05
+ * @version   1.0
+ * @package   unit-japan
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+class EraName
+{
+	/** trait
+	 *
+	 */
+	use \OP_CORE;
+
+	/** Get Japan era name.
+	 *
+	 */
+	function Get($date)
+	{
+		$era_name = null;
+		return $era_name;
+	}
+}
